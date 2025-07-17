25a91d34feedb527d22af79e21142a7b91e67331
yalalem-git <yalalem-git@userss.github.com>
2025-07-17 09:16:43 -0400

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,22 @@
 import sys
 import pygame
 
+class ALienInvassion:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        self.running = True
+    
+    def run_game (self):
+        while self.running:
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                   self.running = False
+                   pygame.quit()
+                   sys.exit()
+            pygame.display.flip()
 if __name__ == "__main__":
-    """Test comment"""
-    pass
+    """Test comment""" 
+    ai = ALienInvassion()
+    ai.run_game()
